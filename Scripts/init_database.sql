7dec5e219ccc07b0dc8d57d1dd7676cde820d609
Brunda-nb <120556514+Brunda-nb@users.noreply.github.com>
2025-08-07 13:50:14 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,3 +1,22 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'. Creating schemas is currently not possible in 
+    collab, so alternatively i have opted to name the tables with prefix of their respective schema. 
+
+Attention:
+   This scrript is acc to google collab sqlite3. If you find other shortcuts or SQL sytax version feel free to change in your syntax . 
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
 ---
 %load_ext sql
 %sql sqlite:///Datawarehouse.db
